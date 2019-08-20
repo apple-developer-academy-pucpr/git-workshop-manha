9b3cc441fa031d776d70bd5f7695942e2915cdd8
Maria Fernanda Azolin <azolin.mf@gmail.com>
2019-08-20 11:12:54 -0300

    add mafe screen

---
+++ GitWorkshop/GitWorkshop/MafeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MafeViewController.swift
+//  GitWorkshop
+//
+//  Created by Maria Fernanda Azolin on 20/08/19.
+//  Copyright © 2019 Luiz Veloso. All rights reserved.
+//
+
+import UIKit
+
+class MafeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
